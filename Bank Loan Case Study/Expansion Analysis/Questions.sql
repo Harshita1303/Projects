7bde740671af0c77638ae9b207a12f466c0f5174
Harshita <110682759+Harshita1303@users.noreply.github.com>
2025-01-23 00:49:01 +0530

    Add files via upload

---
+++ Bank Loan Case Study/Expansion Analysis/Questions.sql
@@ -0,0 +1,9 @@
+/*Key Questions
+Average Sale vs Rent
+Find each city and their average sale per customer and avg rent per customer
+
+Monthly Sales Growth
+Sales growth rate: Calculate the percentage growth (or decline) in sales over different time periods (monthly).
+
+Market Potential Analysis
+Identify top 3 city based on highest sales, return city name, total sale, total rent, total customers, estimated coffee consumer*/
